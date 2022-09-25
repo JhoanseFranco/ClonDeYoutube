24baf3a2b5c6d0fedeca61f1d06eacfb10724c62
jhoan franco <jhoanse.dev@gmail.com>
2022-09-25 08:20:01 -0500

    Se crea proyecto y se agregan iconos, splash y set de colores

---
+++ ClonDeYoutube/ClonDeYoutube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClonDeYoutube
+//
+//  Created by jhoan sebastian franco cardona on 25/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
